6c2660b142264e29e1ea035433dc5e48281c739d
Christopher Mazey <cjmazey@gmail.com>
2015-03-01 17:08:12 -0500

    fin Comonad

--- src/Course/Comonad.hs
+++ src/Course/Comonad.hs
@@ -29,20 +29,13 @@ class Extend f => Comonad f where
 -- >>> copure (Id 7)
 -- 7
 instance Comonad Id where
-  copure ::
-    Id a
-    -> a
-  copure =
-    error "todo"
+  copure :: Id a -> a
+  copure = runId
 
 -- | Witness that all things with (<<=) and copure also have (<$>).
 --
 -- >>> (+10) <$> Id 7
 -- Id 17
-(<$>) ::
-  Comonad f =>
-  (a -> b)
-  -> f a
-  -> f b
-(<$>) =
-  error "todo"
+(<$>) :: Comonad f
+      => (a -> b) -> f a -> f b
+f <$> x = f . copure <<= x
