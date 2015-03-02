60f07212a0cabef818f5784811d0c03eb8e59558
Christopher Mazey <cjmazey@gmail.com>
2015-03-01 20:22:47 -0500

    fin Compose

--- src/Course/Compose.hs
+++ src/Course/Compose.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE InstanceSigs #-}
 
 module Course.Compose where
 
@@ -13,27 +14,29 @@ import Course.Bind
 
 newtype Compose f g a =
   Compose (f (g a))
+  deriving Show
 
 -- Implement a Functor instance for Compose
-instance (Functor f, Functor g) =>
-    Functor (Compose f g) where
-  (<$>) =
-    error "todo"
+instance (Functor f,Functor g) => Functor (Compose f g) where
+  (<$>) :: (a -> b) -> Compose f g a -> Compose f g b
+  h <$> (Compose x) =
+    Compose $
+    ((<$>) . (<$>)) h x
 
-instance (Apply f, Apply g) =>
-  Apply (Compose f g) where
 -- Implement the (<*>) function for an Apply instance for Compose
-  (<*>) =
-    error "todo"
+instance (Apply f,Apply g) => Apply (Compose f g) where
+  (<*>) :: Compose f g (a -> b) -> Compose f g a -> Compose f g b
+  (Compose h) <*> (Compose x) = Compose $ (<*>) <$> h <*> x
 
-instance (Applicative f, Applicative g) =>
-  Applicative (Compose f g) where
 -- Implement the pure function for an Applicative instance for Compose
-  pure =
-    error "todo"
+instance (Applicative f,Applicative g) => Applicative (Compose f g) where
+  pure :: a -> Compose f g a
+  pure x =
+    Compose $
+    (pure . pure) x
 
-instance (Bind f, Bind g) =>
-  Bind (Compose f g) where
 -- Implement the (=<<) function for a Bind instance for Compose
-  (=<<) =
+instance (Bind f, Bind g) => Bind (Compose f g) where
+  (=<<) :: (a -> Compose f g b) -> Compose f g a -> Compose f g b
+  k =<< (Compose m) =
     error "todo"
