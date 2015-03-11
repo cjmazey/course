01e09cde854901a85d990dee235b8a5ae2daa30c
Christopher Mazey <cjmazey@gmail.com>
2015-03-11 15:02:52 -0400

    still slow fast anagram

--- src/Course/FastAnagrams.hs
+++ src/Course/FastAnagrams.hs
@@ -10,16 +10,13 @@ import qualified Data.Set as S
 
 -- Return all anagrams of the given string
 -- that appear in the given dictionary file.
-fastAnagrams ::
-  Chars
-  -> Filename
-  -> IO (List Chars)
+fastAnagrams :: Chars -> Filename -> IO (List Chars)
 fastAnagrams s f =
   let p :: S.Set NoCaseString
-      p = (S.fromList . hlist) (NoCaseString <$> permutations s)
+      p = S.fromList . hlist . (NoCaseString <$>) . permutations $ s
       d :: IO (S.Set NoCaseString)
-      d = (S.fromList . hlist) <$> ((NoCaseString <$>) <$> lines <$> readFile f)
-  in (ncString <$>) <$> ((listh . S.toList) <$> (S.intersection p <$> d))
+      d = S.fromList . hlist . (NoCaseString <$>) . lines <$> readFile f
+  in (ncString <$>) . listh . S.toList . S.intersection p <$> d
 
 newtype NoCaseString =
   NoCaseString {
