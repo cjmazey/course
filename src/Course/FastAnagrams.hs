60401e6f97e24310b78286a251135129e18de805
Christopher Mazey <cjmazey@gmail.com>
2015-03-11 02:16:25 -0400

    slow fast anagrams

--- src/Course/FastAnagrams.hs
+++ src/Course/FastAnagrams.hs
@@ -14,8 +14,12 @@ fastAnagrams ::
   Chars
   -> Filename
   -> IO (List Chars)
-fastAnagrams =
-  error "todo"
+fastAnagrams s f =
+  let p :: S.Set NoCaseString
+      p = (S.fromList . hlist) (NoCaseString <$> permutations s)
+      d :: IO (S.Set NoCaseString)
+      d = (S.fromList . hlist) <$> ((NoCaseString <$>) <$> lines <$> readFile f)
+  in (ncString <$>) <$> ((listh . S.toList) <$> (S.intersection p <$> d))
 
 newtype NoCaseString =
   NoCaseString {
@@ -27,3 +31,6 @@ instance Eq NoCaseString where
 
 instance Show NoCaseString where
   show = show . ncString
+
+instance Ord NoCaseString where
+  compare = compare `on` map toLower . ncString
