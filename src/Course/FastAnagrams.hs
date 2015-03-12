95b995ac080eeea57a2c00e7fa7ad980a7a0a111
Christopher Mazey <cjmazey@gmail.com>
2015-03-11 21:41:55 -0400

    almost fast fast anagram

--- src/Course/FastAnagrams.hs
+++ src/Course/FastAnagrams.hs
@@ -7,16 +7,37 @@ import Course.Core
 import Course.List
 import Course.Functor
 import qualified Data.Set as S
+import qualified Data.Map as M
+
+
+sort :: Ord a
+     => List a -> List a
+sort Nil = Nil
+sort (h :. t) =
+  sort (filter (< h) t) ++
+  (h :. Nil) ++
+  sort (filter (>= h) t)
+
+-- Create an anagram map from a file.  The file should be a dictionary
+-- containing one word per line, e.g., "/usr/share/dict/cracklib-small."
+makeMap :: Filename -> IO (M.Map Chars (List Chars))
+makeMap f =
+  foldRight (\w m ->
+               M.insertWith (++)
+                            (sort w)
+                            (w :. Nil)
+                            m)
+            M.empty .
+  lines <$>
+  readFile f
 
 -- Return all anagrams of the given string
 -- that appear in the given dictionary file.
 fastAnagrams :: Chars -> Filename -> IO (List Chars)
 fastAnagrams s f =
-  let p :: S.Set NoCaseString
-      p = S.fromList . hlist . (NoCaseString <$>) . permutations $ s
-      d :: IO (S.Set NoCaseString)
-      d = S.fromList . hlist . (NoCaseString <$>) . lines <$> readFile f
-  in (ncString <$>) . listh . S.toList . S.intersection p <$> d
+  M.findWithDefault (s :. Nil)
+                    ((sort . (toLower <$>)) s) <$>
+  makeMap f
 
 newtype NoCaseString =
   NoCaseString {
@@ -28,6 +49,3 @@ instance Eq NoCaseString where
 
 instance Show NoCaseString where
   show = show . ncString
-
-instance Ord NoCaseString where
-  compare = compare `on` map toLower . ncString
